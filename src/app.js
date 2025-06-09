01b0b9d757c0f0757f1c23836ee7bf20fa7c5457
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2025-06-09 23:17:10 +0530

    init database and signup api with dummy data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const connectDb = require('./config/database')
-
+const User = require('./models/user');
 const app = express();
 
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "khush",
+        lastName: "pandey",
+        emailId: "khush@example.com",
+        password: "password123",
+        age: "25",
+        number: "1234567890"
+    })
+    try {
+        await user.save();
+        res.send("User registered successfully")
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Error Saving the User : " + err.message);
+    }
+
+})
 
 connectDb().then(() => {
     console.log("Database connected successfully...")
