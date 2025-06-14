d425c24774498747a66cd06be4f394db5da8fafa
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2025-06-14 23:54:11 +0530

    added token and cookies expire code

--- src/app.js
+++ src/app.js
@@ -39,8 +39,8 @@ app.post("/login", async (req, res) => {
         const isPasswordValid = await bcrypt.compare(password, user.password)
         if (isPasswordValid) {
             // Create a JWT Token
-            const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$789");
-            res.cookie("token", token);
+            const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$789", { expiresIn: "7d" });
+            res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
             res.send("Login SuccessFull!!!");
         } else {
             throw new Error("Password is not Correct");
@@ -63,81 +63,6 @@ app.get("/profile", userAuth, async (req, res) => {
         res.status(400).send(err.message || "Something went wrong");
     }
 });
-
-
-// get user by email
-app.post("/user", async (req, res) => {
-    const userEmail = req.body.emailId
-    try {
-        const users = await User.find({ emailId: userEmail })
-        if (users.length === 0) {
-            res.status(404).send("User Not found")
-        } else {
-            res.send(users);
-        }
-    } catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-})
-
-// Feed Api - GET/Feed - get all the users from the db
-app.get("/feed", async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-})
-
-// Delete a user from db
-app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
-    try {
-        const user = await User.findByIdAndDelete(userId)
-        res.send("users deleted Successfully")
-    } catch (err) {
-        res.status(400).send("Something went wrong")
-    }
-})
-
-// Update the data of user
-app.patch("/user/:userId", async (req, res) => {
-    const userId = req.params.userId;
-    const data = req.body;
-
-    try {
-        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
-        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
-
-        if (!isUpdateAllowed) {
-            throw new Error("Update not allowed");
-        }
-
-        if (data.skills && data.skills.length > 10) {
-            throw new Error("Skills can not be more than 10");
-        }
-
-        const user = await User.findByIdAndUpdate(userId, data, {
-            new: true,
-            runValidators: true
-        });
-
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-
-        res.send("User updated successfully");
-    } catch (err) {
-        console.error(err);
-        res.status(400).send(err.message || "Something went wrong");
-    }
-});
-
-
-
-
-
 
 connectDb().then(() => {
     console.log("Database connected successfully...")
